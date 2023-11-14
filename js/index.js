51ab24273c567fce9e60ec5e27442d03f09e372f
Hayley Witherell <hayleywitherell@gmail.com>
2023-11-14 16:35:52 -0700

    Created list of skills

--- js/index.js
+++ js/index.js
@@ -11,3 +11,18 @@ const copyright = document.createElement("p");
 copyright.innerHTML = `Hayley ${thisYear}`
 
 footer.appendChild(copyright);
+
+// skills
+
+const skills = ["JavaScript", "HTML", "CSS", "React", "Ruby on Rails"];
+
+const skillsSection = document.getElementById("skills");
+
+const skillsList = skillsSection.querySelector("ul")
+
+for (let i = 0; i < skills.length; i++) {
+  const skill = document.createElement("li");
+
+  skill.innerText = skills[i];
+  skillsList.appendChild(skill);
+}
