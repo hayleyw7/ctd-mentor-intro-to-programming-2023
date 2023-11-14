e9084fc68d815ef2d7ec6c3db7b5a78ee3174fec
Hayley Witherell <hayleywitherell@gmail.com>
2023-11-14 16:19:34 -0700

    Inserted copyright test in footer

--- js/index.js
+++ js/index.js
@@ -1,25 +1,13 @@
 // copyright
 
 const today = new Date();
+
 const thisYear = today.getFullYear();
 
 const footer = document.querySelector("footer");
 
 const copyright = document.createElement("p");
 
-copyright.innerHTML = `Hayley ${thisYear}`;
+copyright.innerHTML = `Hayley ${thisYear}`
+
 footer.appendChild(copyright);
-
-// skills
-
-const skills = ["JavaScript", "React", "HTML/CSS", "Ruby on Rails"];
-
-const skillsSection = document.getElementById("skills");
-const skillsList = skillsSection.querySelector("ul");
-
-for (let i = 0; i < skills.length; i++) {
-  const skill = document.createElement("li");
-
-  skill.innerText = skills[i];
-  skillsList.appendChild(skill);
-};
